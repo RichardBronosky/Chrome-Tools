6eed17c255309d3264d6c8c9ee505e400b153a7e
Richard Bronosky <richard.bronosky@cmgdigital.com>
2010-08-10 00:09:15 -0400

    initial github offering

---
+++ chromium-updater.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+TMP_DIR=/tmp/chromiumdownload
+APP_DIR=/Applications
+BASE_URL=http://build.chromium.org/buildbot/snapshots/chromium-rel-mac
+LATEST_VER=$(curl -sS $BASE_URL/LATEST)
+# Either of these could change at any moment.
+FILE_NAME=chrome-mac.zip
+APP_NAME=Chromium.app
+
+# Check current revision number.
+CURRENT_VER=$(/usr/libexec/PlistBuddy -c "print SVNRevision" $APP_DIR/$APP_NAME/Contents/Info.plist) || CURRENT_VER=0
+# Bail if there is not a newer version.
+echo "Latest is $LATEST_VER. You have $CURRENT_VER."
+[[ $LATEST_VER > $CURRENT_VER ]] && echo "Downloading." || { echo "Quitting."; exit 1; }
+
+mkdir -p $TMP_DIR && cd $TMP_DIR
+curl -O $BASE_URL/$LATEST_VER/$FILE_NAME
+# I really wish they would create tar/gz files instead of zip files so that I could pipe curl to tar and not write the archive to disk.
+unzip -qq $FILE_NAME
+# Because this script rename the existing app with its version and moves it to the temp dir, it is safe to run while the app open.`
+[[ -d $APP_DIR/$APP_NAME ]] && mv $APP_DIR/$APP_NAME ./${APP_NAME%%.app}.$CURRENT_VER.app
+mv ${FILE_NAME%%.zip}/$APP_NAME $APP_DIR/$APP_NAME
+
