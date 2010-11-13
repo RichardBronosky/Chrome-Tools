2e5f8bd453aebde59e5fde58baf17eb80f7cb470
Richard Bronosky <richard.bronosky@cmgdigital.com>
2010-11-13 12:41:31 -0500

    added the -o flag to open after downloading.

--- chromium-updater.sh
+++ chromium-updater.sh
@@ -24,3 +24,4 @@ unzip -qq $FILE_NAME
 [[ -d $APP_DIR/$APP_NAME ]] && mv $APP_DIR/$APP_NAME ./${APP_NAME%%.app}.$CURRENT_VER.app
 mv ${FILE_NAME%%.zip}/$APP_NAME $APP_DIR/$APP_NAME
 
+[[ $1 == '-o' ]] && open $APP_DIR/$APP_NAME
