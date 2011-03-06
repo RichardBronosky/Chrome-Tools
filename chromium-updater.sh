f7ad85dbf98526db9dd2ad29574480431adfa5d2
Yaroslav Shirokov <Yaroslav.Shirokov@cmgdigital.com>
2011-03-05 22:53:09 -0500

    [master] Updated with pasting the changelog

--- chromium-updater.sh
+++ chromium-updater.sh
@@ -4,16 +4,19 @@ set -o errexit
 
 TMP_DIR=/tmp/chromiumdownload
 APP_DIR=/Applications
-BASE_URL=http://build.chromium.org/f/chromium/snapshots/chromium-rel-mac
+BASE_URL=http://build.chromium.org/f/chromium/snapshots/chromium-rel-mac/
 LATEST_VER=$(curl -sS $BASE_URL/LATEST)
 # Either of these could change at any moment.
 FILE_NAME=chrome-mac.zip
 APP_NAME=Chromium.app
 
-# Check current revision number.
+# check current revision number
 CURRENT_VER=$(/usr/libexec/PlistBuddy -c "print SVNRevision" $APP_DIR/$APP_NAME/Contents/Info.plist) || CURRENT_VER=0
-# Bail if there is not a newer version.
+CHANGELOG="http://build.chromium.org/buildbot/perf/dashboard/ui/changelog.html?url=/trunk/src&range=$LATEST_VER:$CURRENT_VER&mode=html"
+echo $CHANGELOG | pbcopy
+# bail if there is not a newer version
 echo "Latest is $LATEST_VER. You have $CURRENT_VER."
+echo "Changelog (in clipboard): $CHANGELOG"
 [[ $LATEST_VER > $CURRENT_VER ]] && echo "Downloading." || { echo "Quitting."; exit 1; }
 
 mkdir -p $TMP_DIR && cd $TMP_DIR
@@ -21,6 +24,7 @@ curl -O $BASE_URL/$LATEST_VER/$FILE_NAME
 # I really wish they would create tar/gz files instead of zip files so that I could pipe curl to tar and not write the archive to disk.
 unzip -qq $FILE_NAME
 # Because this script rename the existing app with its version and moves it to the temp dir, it is safe to run while the app open.`
+APP_NAME=$(basename ${FILE_NAME%%.zip}/*.app)
 [[ -d $APP_DIR/$APP_NAME ]] && mv $APP_DIR/$APP_NAME ./${APP_NAME%%.app}.$CURRENT_VER.app
 mv ${FILE_NAME%%.zip}/$APP_NAME $APP_DIR/$APP_NAME
 
